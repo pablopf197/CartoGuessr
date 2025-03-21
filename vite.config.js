de958ec07e41720d8b7e9fe173467186c634a74f
pablopf197 <pardofernandezpablo@gmail.com>
2025-03-21 19:50:23 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command, mode }) => {
   return {
     plugins: [react()], 
-    base: mode === 'production' ? '/cartoguessr/' : '/',
+    base: '/',
     build: {
       outDir: 'dist',
       rollupOptions: {
